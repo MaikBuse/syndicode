78c4d25740aea1004fd030ec8d86ec6814acdbdc
Maik Buse <contact@maikbuse.com>
2025-07-21 20:33:56 +0200

    fix(infra): use WEUR as a location for the r2 bucket

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -7,7 +7,7 @@ data "cloudflare_zone" "main" {
 resource "cloudflare_r2_bucket" "assets" {
   account_id = var.cloudflare_account_id
   name       = var.r2_bucket_name
-  location   = "auto"
+  location   = "WEUR"
 }
 
 # Custom domain for R2 bucket
